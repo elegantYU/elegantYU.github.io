23cd6b341dc97800043c9dd626561ebae03e49b8
111 <a111@cmm.lan>
2019-09-04 10:51:06 +0800

    老子的打字共话

--- themes/Material-T/source/js/main.js
+++ themes/Material-T/source/js/main.js
@@ -1,16 +1,26 @@
 $(document).ready(function () {
-  // var navbar = $("#navbar");
-  // $(window).scroll(function(){
-  //   if(navbar.offset().top > 0) {
-  //     navbar.addClass("navbar-light");
-  //     navbar.removeClass("navbar-dark");
-  //   } else {
-  //     navbar.addClass("navbar-dark");
-  //     navbar.removeClass("navbar-light");
-  //   }
-  // })
-  $('#navbar-toggler-btn').on('click', function () {
-    $('.animated-icon').toggleClass('open');
-    $('#navbar').toggleClass('navbar-col-show');
-  });
+  const navbarIcon = () => {
+    $('#navbar-toggler-btn').on('click', function () {
+      $('.animated-icon').toggleClass('open');
+      $('#navbar').toggleClass('navbar-col-show');
+    });
+  }
+  // 打字机器_文字
+  const typeMachine = () => {
+    let i = 0
+    const typingBox = document.getElementsByClassName('wow')[0].children[0]
+    const typingWords = typingBox.innerText
+    typingBox.innerText = ''
+    const typing = setInterval(() => {
+      typingBox.innerHTML += typingWords[i++]
+      i >= typingWords.length && clearInterval(typing)
+    }, 200)
+  }
+
+  const init = () => {
+    navbarIcon()
+    typeMachine()
+  }
+
+  init()
 });
